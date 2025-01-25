1b44092df1219cfe5190529bfc1a7a64070ebf80
kapitu <ketkap007@gmail.com>
2025-01-26 00:52:29 +0200

    fix-hw-nodeJs

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,40 +1,18 @@
 
-import express from 'express';
-import { getContacts, getContactById } from '../services/contacts.js';
+import { Router } from 'express';
+import { getContactsController,
+            getContactByIdController ,
+            createContactController,
+            patchContactController,
+            deleteContactController  } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
-const router = express.Router();
-router.get('/', async (req, res) => {
-  try {
-    const contacts = await getContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
+const router = Router();
 
-router.get('/:contactId', async (req, res) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      return res.status(404).json({ message: 'Contact not found' });
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
+router.get('/',ctrlWrapper(getContactsController));
+router.get('/:contactId',ctrlWrapper(getContactByIdController));
+router.post('/',ctrlWrapper(createContactController));
+router.patch('/:contactId', ctrlWrapper(patchContactController));
+router.delete('/:contactId', ctrlWrapper(deleteContactController ))
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,22 +1,51 @@
-import express from 'express';
+import express, { application, json } from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import contactsRouter from './routes/contacts.js';
-
-export function setupServer() {
- const app = express();
-  app.use(pino());
-  app.use(cors());
-  app.use('/contacts', contactsRouter);
-
- app.use((req, res, next) => {
-    res.status(404).json({ message: 'Not found' });
-});
+import contactsRouter from './routes/contacts.js'; // Імпортуємо роутер
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(process.env.PORT) || 3000;
+
+export function setupServer() {
+  const app = express();// Ініціалізуємо додаток Express
+
+ // Додаємо middleware для обробки JSON
+  app.use(json({
+    type:['application/json', 'application/vnd.api+json']
+  })); 
+
+  app.use(cors());
+
+  app.use(
+    pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+app.use((req, res, next) => {
+  console.log(`Received request: ${req.method} ${req.originalUrl}`); // Логуємо шлях та метод запиту
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!',
+  });
+});
+// Роут для /contacts
+  app.use('/contacts', contactsRouter);
+
+  // Middleware для обробки 404
+  app.use(notFoundHandler);
+
+// Middleware для обробки помилок
+app.use(errorHandler);
+
  app.listen(PORT, () => {
    console.log(`Server is running on ${PORT}`);
- });
- 
+ }); 
 };
 
